be22c48a4a5df10149d05545f40769813f6375f5
Jose Manuel Rodriguez Guerreiro <joro@ciklum.com>
2022-10-14 14:17:45 +0200

    fix(scoreboard): fixed sorting error due to array mutation

--- src/app/shared/pipes/match-list-sort/match-list-sort.pipe.ts
+++ src/app/shared/pipes/match-list-sort/match-list-sort.pipe.ts
@@ -4,21 +4,26 @@ import { Match } from 'src/app/core/data/models/match.model';
 @Pipe({ name: 'matchListSort' })
 export class MatchListSortPipe implements PipeTransform {
   transform(records: Match[] | null): Match[] {
-    return records && records.length > 0
-      ? records.sort((m1, m2) => {
-          const m1Score = m1.score.home + m1.score.away;
-          const m2Score = m2.score.home + m2.score.away;
-          const m1Index = records.indexOf(m1);
-          const m2Index = records.indexOf(m2);
+    let sortedRecords: Match[] = [];
 
-          return m1Score === m2Score
-            ? m1Index < m2Index
-              ? 1
-              : -1
-            : m1Score < m2Score
+    if (records && records.length > 0) {
+      sortedRecords = [...records];
+      sortedRecords.sort((m1, m2) => {
+        const m1Score = m1.score.home + m1.score.away;
+        const m2Score = m2.score.home + m2.score.away;
+        const m1Index = records.indexOf(m1);
+        const m2Index = records.indexOf(m2);
+
+        return m1Score === m2Score
+          ? m1Index < m2Index
             ? 1
-            : -1;
-        })
-      : [];
+            : -1
+          : m1Score < m2Score
+          ? 1
+          : -1;
+      });
+    }
+
+    return sortedRecords;
   }
 }
